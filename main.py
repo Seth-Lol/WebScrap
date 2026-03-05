92f3d41d2c5b48d910b676296004a2b8147bdd13
Seth-Lol <investingseth@gmail.com>
2026-03-05 15:46:42 +0700

    Webscrap from Titanic

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
-df = pd.DataFrame({"Test": ["It", "Worked!"]})
+# Create a simple table of data (called a DataFrame)
+data = {
+    "Names": ["Alice", "Bob", "Charlie"],
+    "Ages": [25, 30, 35]
+}
+
+df = pd.DataFrame(data)
+
+print("Pandas is working! Here is your data:")
 print(df)
