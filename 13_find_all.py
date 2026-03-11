fa9866754d4044893d42caf14dbdb3790c555ca4
Seth-Lol <investingseth@gmail.com>
2026-03-11 15:36:02 +0700

    Hw 13 find all to find

--- 13_find_all.py
+++ 13_find_all.py
@@ -11,11 +11,12 @@ soup = BeautifulSoup(content,'lxml')
 box = soup.find('article', class_ = 'main-article')
 
 title = box.find('h1').get_text()
-transcripts  = box.find_all('p', class_ = 'cue-line') # subtitle-cue # .get_text(strip=True, separator='\n')
+transcripts  = box.find_all('p', class_ = 'cue-line')
+transcript_texts = [transcript.get_text(strip=True, separator='\n') for transcript in transcripts]
 
 
 with open(f'{title}_all.txt','w+') as file:
-    for transcript in transcripts:
+    for transcript in transcript_texts:
         file.write("%s\n" % transcript) 
 
 print("End of the program")
